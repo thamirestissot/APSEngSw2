20839540760efda51ae229652fba5fedfd0fe4ec
thamirestissot <thamirestissot3@gmail.com>
2019-06-02 18:46:15 -0300

    inicio

---
+++ aps/aps/Box.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aps
+{
+    public class Box
+    {
+        public string TipoVeiculo { get; set; }
+        public decimal ValorHora { get; set; }
+        public int Numero { get; set; }
+        public bool Ocupado { get; set; }
+    }
+}
